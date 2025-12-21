ff4084f1649567b0d7d7cdb5b4aa7de80114748d
Slogomos <slogo.mos@gmail.com>
2025-12-21 12:07:30 -0600

    commit 0.02

--- src/main.cpp
+++ src/main.cpp
@@ -212,8 +212,8 @@ private:
     std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
     std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
 
-    float queuePriority = 1.0f;
     for (uint32_t queueFamilly : uniqueQueueFamilies) {
+    float queuePriority = 1.0f;
     VkDeviceQueueCreateInfo queueCreateInfo{};
     queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
     queueCreateInfo.queueFamilyIndex = queueFamilly;
@@ -222,7 +222,7 @@ private:
     queueCreateInfos.push_back(queueCreateInfo);
     }
 
-    VkPhysicalDeviceFeatures deviceFeatures;
+    VkPhysicalDeviceFeatures deviceFeatures{};
 
     VkDeviceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
